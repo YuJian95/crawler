124d15cd4cd953c9b4456d2562746658db7115a8
chenlijian <clj9509@163.com>
2020-12-13 21:50:44 +0800

    style(weather.py)：update code nate.

--- cn/yujian95/crawler/weather/weather.py
+++ cn/yujian95/crawler/weather/weather.py
@@ -22,6 +22,7 @@ def crawling_weather_data(url):
         sub_data = text.text.split()
         # 将日期加载到列表中
         dates.append(sub_data[0])
+        # 根据 html 内容获取对应的 文字内容。
         # 索引 1-3 实际上取得是字符串的2,3内容，取不到第4个，join可以把字符串整合一块
         conditions.append(''.join(sub_data[1:3]))
         temperature.append(''.join(sub_data[3:6]))
@@ -47,11 +48,12 @@ def get_year_weather_data(year, city):
             weather_data_of_month.append(
                 crawling_weather_data(
                     'http://www.tianqihoubao.com/lishi/' + city + '/month/' + year + '{}.html'.format(month)))
-        # 链接中补充 0
+        # 当月份为 1 ~ 9 时，链接中补充 0 前缀，例如 01 代表一月
         else:
             weather_data_of_month.append(
                 crawling_weather_data(
                     'http://www.tianqihoubao.com/lishi/' + city + '/month/' + year + '0{}.html'.format(month)))
 
     print("抓取完成，结束时间：" + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    # 拼接 12 个月的天气数据
     return pd.concat(weather_data_of_month).reset_index(drop=True)
